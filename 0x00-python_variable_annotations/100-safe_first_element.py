ae54a1caad9e579e9d68f52f957c5e1cc3e3c863
yNOtOmosh <onyango.omondi.tony@gmail.com>
2024-05-02 20:54:09 +0000

    Augment the following code with the correct duck-typed annotations:
    # The types of the elements of the input are not know
    def safe_first_element(lst):
        if lst:
            return lst[0]
        else:
            return None

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Duck typing - first element of a sequence."""
+from typing import Any, Sequence, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """Returns first element of lst if exist otherwise return None."""
+    if lst:
+        return lst[0]
+    else:
+        return None
