80422b5438e6d52a51eedaebe07b685727894a2e
yNOtOmosh <onyango.omondi.tony@gmail.com>
2024-05-02 20:43:53 +0000

    Annotate the below function’s parameters and return values with the appropriate types
    def element_length(lst):
        return [(i, len(i)) for i in lst]

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Let's duck type an iterable object."""
+from typing import Iterable, Sequence, List, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Return listof tuples containig elements from lst."""
+    return [(i, len(i)) for i in lst]
