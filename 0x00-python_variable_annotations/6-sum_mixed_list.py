de033712925c1cce170f0ede95a76fa6e168bf5c
yNOtOmosh <onyango.omondi.tony@gmail.com>
2024-05-02 20:19:44 +0000

    a type-annotated function sum_mixed_list which takes a list mxd_lst of integers and floats and returns their sum as a float.

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Complex types - mixed list."""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """return sum of list of integers and floats."""
+    return sum(mxd_lst)
